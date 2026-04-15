cdb97a91f21b1ae075ba41e640fcf2a73dc47e22
developer <156831598+softwaredeveloper0@users.noreply.github.com>
2026-04-15 14:35:41 +0530

    Delete taskmanagement directory

--- taskmanagement/src/main/java/com/example/taskmanagement/Exceptions/CustomException.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskmanagement.Exceptions;
-
-public class CustomException extends RuntimeException {
-
-    public CustomException(String message) {
-        super(message);
-    }
-}
